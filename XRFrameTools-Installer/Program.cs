e62e87b868bd0573d9b45258d7456c2872b44ff6
Fred Emmott <git@fredemmott.com>
2024-12-13 18:28:34 -0600

    📦 Use version number JSON in packaging script

--- XRFrameTools-Installer/Program.cs
+++ XRFrameTools-Installer/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System.CommandLine;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
 using WixSharp;
 using WixSharp.CommonTasks;
 using WixSharp.Controls;
 using File = WixSharp.File;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 [assembly: InternalsVisibleTo(assemblyName: "XRFrameTools_Installer.aot")] // assembly name + '.aot suffix
+
+async Task SetProjectVersionFromJson(ManagedProject project, DirectoryInfo inputRoot)
+{
+    var stream = System.IO.File.OpenRead(inputRoot.GetFiles("installer/version.json").First().FullName);
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+        
+    var version = await JsonSerializer.DeserializeAsync<JsonVersionInfo>(stream, options);
+    Debug.Assert(version != null, nameof(version) + " != null");
+    var c = version.Components;
+    project.Version = new Version($"{c.A}.{c.B}.{c.C}.{c.D}");
+    project.OutFileName += $"-v{c.A}.{c.B}.{c.C}";
+    if (!version.Tagged)
+    {
+        project.OutFileName += $"+{version.TweakLabel}.{c.D}";
+    }
+}
 
 async Task<int> CreateMsi(DirectoryInfo inputRoot, string? signingKeyId, string? timestampServer, FileInfo? stampFile)
 {
@@ -26,6 +49,7 @@ async Task<int> CreateMsi(DirectoryInfo inputRoot, string? signingKeyId, string?
                 new Dir("lib", new Files("lib/*.*")),
                 new Dir("share", new Files("share/*.*")),
                 new Files(installerResources, @"installer/*.*")));
+    await SetProjectVersionFromJson(project, inputRoot);
 
     const string apiLayersKey = @"SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit";
     foreach (var file in inputRoot.GetDirectories("lib").First().GetFiles("XR_APILAYER_*64.json"))
@@ -104,3 +128,18 @@ command.Add(stampFileArg);
 command.SetHandler(
     CreateMsi, inputRootArg, signingKeyArg, timestampServerArg, stampFileArg);
 return await command.InvokeAsync(args);
+
+class JsonVersionComponents
+{
+    public int A { get; set; }
+    public int B { get; set; }
+    public int C { get; set; }
+    public int D { get; set; }
+}
+
+class JsonVersionInfo
+{
+    public JsonVersionComponents Components { get; set; } = new JsonVersionComponents();
+    public string TweakLabel { get; set; } = string.Empty;
+    public bool Tagged { get; set; }
+}
