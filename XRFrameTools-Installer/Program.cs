ba4a65c91e076d87938e0f6ccd276f050da02d31
Fred Emmott <git@fredemmott.com>
2024-12-13 18:44:39 -0600

    🚚 Minor cleanup/refactoring of installer builder

--- XRFrameTools-Installer/Program.cs
+++ XRFrameTools-Installer/Program.cs
@@ -11,102 +11,6 @@ using System.Text.Json.Serialization;
 
 [assembly: InternalsVisibleTo(assemblyName: "XRFrameTools_Installer.aot")] // assembly name + '.aot suffix
 
-async Task SetProjectVersionFromJson(ManagedProject project, DirectoryInfo inputRoot)
-{
-    var stream = System.IO.File.OpenRead(inputRoot.GetFiles("installer/version.json").First().FullName);
-    var options = new JsonSerializerOptions
-    {
-        PropertyNameCaseInsensitive = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    };
-        
-    var version = await JsonSerializer.DeserializeAsync<JsonVersionInfo>(stream, options);
-    Debug.Assert(version != null, nameof(version) + " != null");
-    var c = version.Components;
-    project.Version = new Version($"{c.A}.{c.B}.{c.C}.{c.D}");
-    project.OutFileName += $"-v{c.A}.{c.B}.{c.C}";
-    if (!version.Tagged)
-    {
-        project.OutFileName += $"+{version.TweakLabel}.{c.D}";
-    }
-}
-
-async Task<int> CreateMsi(DirectoryInfo inputRoot, string? signingKeyId, string? timestampServer, FileInfo? stampFile)
-{
-    if (!System.IO.File.Exists($"{inputRoot}/bin/XRFrameTools.exe"))
-    {
-        Console.WriteLine($"Cannot find bin/XRFrameTools.exe in INPUT_ROOT ({inputRoot.FullName})");
-        return 1;
-    }
-
-    var installerResources = new Feature("Installer Resources");
-    installerResources.IsEnabled = false;
-
-    var project =
-        new ManagedProject("XRFrameTools",
-            new Dir(@"%ProgramFiles%\XRFrameTools",
-                new Dir("bin", new Files("bin/*.*")),
-                new Dir("lib", new Files("lib/*.*")),
-                new Dir("share", new Files("share/*.*")),
-                new Files(installerResources, @"installer/*.*")));
-    await SetProjectVersionFromJson(project, inputRoot);
-
-    const string apiLayersKey = @"SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit";
-    foreach (var file in inputRoot.GetDirectories("lib").First().GetFiles("XR_APILAYER_*64.json"))
-    {
-        project.AddRegValue(new RegValue(RegistryHive.LocalMachine, apiLayersKey,
-            $"[INSTALLDIR]lib\\{file.Name}", 0));
-    }
-
-    const string appPathsRoot = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
-    project.AddRegValues(
-        new RegValue(
-            RegistryHive.LocalMachine,
-            appPathsRoot + @"\XRFrameTools.exe",
-            string.Empty,
-            @"[INSTALLDIR]bin\XRFrameTools.exe"),
-        new RegValue(
-            RegistryHive.LocalMachine,
-            appPathsRoot + @"\XRFrameTools-binlog-to-csv.exe",
-            string.Empty,
-            @"[INSTALLDIR]bin\binlog-to-csv.exe")
-    );
-
-    project.GUID = Guid.Parse("e3334ff2-9b8f-4f3c-ba25-5f965f3b7dc9");
-    project.Platform = Platform.x64;
-
-    project.SourceBaseDir = inputRoot.FullName;
-
-    project.ControlPanelInfo.Manufacturer = "Fred Emmott";
-    project.LicenceFile = "installer/LICENSE.rtf";
-
-    if (signingKeyId != null)
-    {
-        project.DigitalSignature = new DigitalSignature
-        {
-            CertificateId = signingKeyId,
-            CertificateStore = StoreType.sha1Hash,
-            HashAlgorithm = HashAlgorithmType.sha256,
-            Description = project.OutFileName,
-            TimeUrl = (timestampServer == null) ? null : new Uri(timestampServer),
-        };
-        project.SignAllFiles = true;
-    }
-    else
-    {
-        project.OutFileName += "-UNSIGNED";
-    }
-
-    var outFile = project.BuildMsi();
-    if (stampFile != null)
-    {
-        System.IO.File.WriteAllText(stampFile.FullName, $"{outFile}\n");
-        Console.WriteLine($"Wrote output path '{outFile}' to `{stampFile.FullName}`");
-    }
-
-    return 0;
-}
-
 var inputRootArg = new Argument<DirectoryInfo>(
     name: "INPUT_ROOT",
     description: "Location of files to include in the installer");
@@ -126,8 +30,128 @@ command.Add(signingKeyArg);
 command.Add(timestampServerArg);
 command.Add(stampFileArg);
 command.SetHandler(
-    CreateMsi, inputRootArg, signingKeyArg, timestampServerArg, stampFileArg);
+    CreateInstaller, inputRootArg, signingKeyArg, timestampServerArg, stampFileArg);
 return await command.InvokeAsync(args);
+
+async Task SetProjectVersionFromJson(ManagedProject project, DirectoryInfo inputRoot)
+{
+    var stream = System.IO.File.OpenRead(inputRoot.GetFiles("installer/version.json").First().FullName);
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
+    var version = await JsonSerializer.DeserializeAsync<JsonVersionInfo>(stream, options);
+    Debug.Assert(version != null, nameof(version) + " != null");
+    var c = version.Components;
+    project.Version = new Version($"{c.A}.{c.B}.{c.C}.{c.D}");
+    project.OutFileName += $"-v{c.A}.{c.B}.{c.C}";
+    if (!version.Tagged)
+    {
+        project.OutFileName += $"+{version.TweakLabel}.{c.D}";
+    }
+}
+
+async Task<int> CreateInstaller(DirectoryInfo inputRoot, string? signingKeyId, string? timestampServer, FileInfo? stampFile)
+{
+    if (!System.IO.File.Exists($"{inputRoot}/bin/XRFrameTools.exe"))
+    {
+        Console.WriteLine($"Cannot find bin/XRFrameTools.exe in INPUT_ROOT ({inputRoot.FullName})");
+        return 1;
+    }
+
+    var project = CreateProject(inputRoot);
+    await SetProjectVersionFromJson(project, inputRoot);
+
+    RegisterAPILayers(inputRoot, project);
+    AddCommandLineAliases(project);
+
+    SignProject(project, signingKeyId, timestampServer);
+    BuildMsi(project, stampFile);
+
+    return 0;
+}
+
+void SignProject(ManagedProject managedProject, string? s, string? timestampServer1)
+{
+    if (s != null)
+    {
+        managedProject.DigitalSignature = new DigitalSignature
+        {
+            CertificateId = s,
+            CertificateStore = StoreType.sha1Hash,
+            HashAlgorithm = HashAlgorithmType.sha256,
+            Description = managedProject.OutFileName,
+            TimeUrl = (timestampServer1 == null) ? null : new Uri(timestampServer1),
+        };
+        managedProject.SignAllFiles = true;
+    }
+    else
+    {
+        managedProject.OutFileName += "-UNSIGNED";
+    }
+}
+
+void BuildMsi(ManagedProject managedProject, FileInfo? fileInfo)
+{
+    var outFile = managedProject.BuildMsi();
+    if (fileInfo != null)
+    {
+        System.IO.File.WriteAllText(fileInfo.FullName, $"{outFile}\n");
+        Console.WriteLine($"Wrote output path '{outFile}' to `{fileInfo.FullName}`");
+    }
+}
+
+ManagedProject CreateProject(DirectoryInfo inputRoot)
+{
+    var installerResources = new Feature("Installer Resources");
+    installerResources.IsEnabled = false;
+
+    var project =
+        new ManagedProject("XRFrameTools",
+            new Dir(@"%ProgramFiles%\XRFrameTools",
+                new Dir("bin", new Files("bin/*.*")),
+                new Dir("lib", new Files("lib/*.*")),
+                new Dir("share", new Files("share/*.*")),
+                new Files(installerResources, @"installer/*.*")));
+
+    project.GUID = Guid.Parse("e3334ff2-9b8f-4f3c-ba25-5f965f3b7dc9");
+    project.Platform = Platform.x64;
+
+    project.SourceBaseDir = inputRoot.FullName;
+
+    project.ControlPanelInfo.Manufacturer = "Fred Emmott";
+    project.LicenceFile = "installer/LICENSE.rtf";
+    return project;
+}
+
+void RegisterAPILayers(DirectoryInfo directoryInfo, ManagedProject managedProject)
+{
+    const string apiLayersKey = @"SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit";
+    foreach (var file in directoryInfo.GetDirectories("lib").First().GetFiles("XR_APILAYER_*64.json"))
+    {
+        managedProject.AddRegValue(new RegValue(RegistryHive.LocalMachine, apiLayersKey,
+            $"[INSTALLDIR]lib\\{file.Name}", 0));
+    }
+}
+
+void AddCommandLineAliases(ManagedProject managedProject)
+{
+    const string appPathsRoot = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+    managedProject.AddRegValues(
+        new RegValue(
+            RegistryHive.LocalMachine,
+            appPathsRoot + @"\XRFrameTools.exe",
+            string.Empty,
+            @"[INSTALLDIR]bin\XRFrameTools.exe"),
+        new RegValue(
+            RegistryHive.LocalMachine,
+            appPathsRoot + @"\XRFrameTools-binlog-to-csv.exe",
+            string.Empty,
+            @"[INSTALLDIR]bin\binlog-to-csv.exe")
+    );
+}
 
 class JsonVersionComponents
 {
