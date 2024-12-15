7ca83f6adfeda436d300d832a11c7b497055171f
Fred Emmott <git@fredemmott.com>
2024-12-15 17:04:32 -0600

    ✅ Fix copying of updater exe
    Can't use generator expressions in configure-time actions.

--- third-party/vicius.cmake
+++ third-party/vicius.cmake
@@ -1,22 +1,25 @@
 include(ExternalProject)
 
-set(UPDATER_EXE_FILENAME "fredemmott_XRFrameTools_Updater.exe")
+set(UPDATER_EXE "fredemmott_XRFrameTools_Updater.exe")
 
-set(
-  UPDATER_EXE_PATH
-  "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${UPDATER_EXE_FILENAME}"
-)
-file(MAKE_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 file(
   DOWNLOAD
   "https://github.com/fredemmott/autoupdates/releases/download/vicius-v1.7.813%2Bfredemmott.3/Updater-Release.exe"
-  "${UPDATER_EXE_PATH}"
+  "${CMAKE_CURRENT_BINARY_DIR}/${UPDATER_EXE}"
   EXPECTED_HASH SHA256=3826B6461A9B865DB9307AECED11F33B07A21A4F9A849DBAA07C9C1DE9F012AC
 )
-install(
-  FILES
-  "${UPDATER_EXE_PATH}"
-  DESTINATION bin
+add_custom_target(
+  vicius
+  ALL
+  COMMAND
+  "${CMAKE_COMMAND}"
+  -E copy_if_different
+  "${CMAKE_CURRENT_BINARY_DIR}/${UPDATER_EXE}"
+  "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${UPDATER_EXE}"
 )
 
-return(PROPAGATE UPDATER_EXE_PATH UPDATER_EXE_FILENAME)
+install(
+  FILES
+  "${CMAKE_CURRENT_BINARY_DIR}/${UPDATER_EXE}"
+  DESTINATION bin
+)
