f498728b3e5953c7f070d341a978898c66ed4765
Fred Emmott <git@fredemmott.com>
2024-12-19 09:15:46 -0600

    💄 Swap order of 'frame interval' and 'render gpu'

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -654,16 +654,16 @@ void MainWindow::PlotFrameTimings(const double maxMicroseconds) {
       mLiveData.mChartFrames.data(),
       mLiveData.mChartFrames.size());
 
+    ImPlot::PlotLineG(
+      "Render GPU",
+      &LiveData::PlotMicroseconds<&AggregatedFrameMetrics::mRenderGpu>,
+      mLiveData.mChartFrames.data(),
+      mLiveData.mChartFrames.size());
+
     ImPlot::HideNextItem(ImPlotCond_Once);
     ImPlot::PlotLineG(
       "Frame Interval",
       &LiveData::PlotMicroseconds<&AggregatedFrameMetrics::mSincePreviousFrame>,
-      mLiveData.mChartFrames.data(),
-      mLiveData.mChartFrames.size());
-
-    ImPlot::PlotLineG(
-      "Render GPU",
-      &LiveData::PlotMicroseconds<&AggregatedFrameMetrics::mRenderGpu>,
       mLiveData.mChartFrames.data(),
       mLiveData.mChartFrames.size());
   }
