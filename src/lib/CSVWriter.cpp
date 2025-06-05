e093e19aca7bc7bfe5f832316f552fbfa9691c33
Fred Emmott <git@fredemmott.com>
2025-06-05 08:33:20 -0500

    🚚 Refactor CSVWriter to support variable columns in the future
    Needed for per-nvenc-session and per-core metrics
    
    refs #42

--- src/lib/CSVWriter.cpp
+++ src/lib/CSVWriter.cpp
@@ -7,6 +7,7 @@
 #include <nvapi.h>
 #include <wil/filesystem.h>
 
+#include <functional>
 #include <ranges>
 
 #include "MetricsAggregator.hpp"
@@ -19,66 +20,71 @@ auto ECFromWin32(DWORD value) {
   return std::error_code {HRESULT_FROM_WIN32(value), std::system_category()};
 }
 
-template <detail::CompileTimeStringHelper V>
-consteval auto operator"" _cl() {
-  return V;
-}
-
 enum class ColumnUnit {
   Counter,
   Micros,
   Bytes,
   KHz,
-  RPM,
   Opaque,
   Boolean,
 };
 
-template <auto TName, ColumnUnit TUnit, auto TGetter>
-struct Column {
-  static constexpr auto Name = TName;
-  static constexpr auto Unit = TUnit;
-  static constexpr auto Getter = TGetter;
+template <class T, class TRet = std::string, class TParam = FrameMetrics>
+concept Getter = std::invocable<T, TParam>
+  && std::convertible_to<std::invoke_result_t<T, TParam>, TRet>;
+template <class T, class TParam = FrameMetrics>
+concept NumericGetter = Getter<T, std::invoke_result_t<T, TParam>, TParam>
+  && (std::integral<std::decay_t<std::invoke_result_t<T, TParam>>>
+      || std::floating_point<std::decay_t<std::invoke_result_t<T, TParam>>>);
 
-  static std::string GetHeader()
-    requires(TUnit == ColumnUnit::Micros)
-  {
-    return std::format("{} (µs)", std::string_view {TName});
+class Column {
+ public:
+  Column() = delete;
+  Column(std::string_view name, ColumnUnit unit, Getter auto getter)
+    : mName(name), mUnit(unit), mGetter([getter](const FrameMetrics& fm) {
+        return std::invoke(getter, fm);
+      }) {
   }
 
-  static std::string GetHeader()
-    requires(TUnit == ColumnUnit::KHz)
-  {
-    return std::format("{} (KHz)", std::string_view {TName});
+  Column(std::string_view name, ColumnUnit unit, NumericGetter auto getter)
+    : Column(name, unit, [getter](const FrameMetrics& fm) {
+        return std::to_string(std::invoke(getter, fm));
+      }) {
   }
 
-  static std::string GetHeader() {
-    return std::string {std::string_view {TName}};
+  Column(std::string_view name, Getter<std::chrono::microseconds> auto getter)
+    : Column(name, ColumnUnit::Micros, [getter](const FrameMetrics& fm) {
+        return std::to_string(std::invoke(getter, fm).count());
+      }) {
   }
 
-  static auto GetValue(const FrameMetrics& afm) {
-    return ConvertValue(GetRawValue(afm));
+  Column(
+    std::string_view name,
+    Getter<uint64_t, DXGI_QUERY_VIDEO_MEMORY_INFO> auto getter)
+    : Column(name, ColumnUnit::Bytes, [getter](const FrameMetrics& fm) {
+        return std::to_string(std::invoke(getter, fm.mVideoMemoryInfo));
+      }) {
+  }
+
+  std::string GetHeader() const {
+    switch (mUnit) {
+      case ColumnUnit::Micros:
+        return std::format("{} (µs)", mName);
+      case ColumnUnit::KHz:
+        return std::format("{} (KHz)", mName);
+      default:
+        return mName;
+    }
+  }
+
+  std::string GetValue(const FrameMetrics& afm) const {
+    return std::invoke(mGetter, afm);
   }
 
  private:
-  static auto GetRawValue(const FrameMetrics& afm) {
-    if constexpr (std::invocable<decltype(TGetter), FrameMetrics>) {
-      return std::invoke(TGetter, afm);
-    } else if constexpr (std::invocable<
-                           decltype(TGetter),
-                           DXGI_QUERY_VIDEO_MEMORY_INFO>) {
-      return std::invoke(TGetter, afm.mVideoMemoryInfo);
-    }
-  }
-
-  template <class T>
-  static constexpr auto ConvertValue(T value) {
-    if constexpr (std::same_as<std::decay_t<T>, std::chrono::microseconds>) {
-      return std::format("{}", value.count());
-    } else {
-      return std::format("{}", value);
-    }
-  }
+  std::string mName;
+  ColumnUnit mUnit;
+  std::function<std::string(const FrameMetrics&)> mGetter;
 };
 
 template <FramePerformanceCounters::ValidDataBits TBit>
@@ -97,107 +103,141 @@ bool HasAnyOfGPUPerfDecreaseBits(const FrameMetrics& frame) {
   return false;
 }
 
-using Row = std::tuple<
-  Column<
-    "Display XrTime"_cl,
+const auto BaseColumns = std::array {
+  Column {
+    "Display XrTime",
     ColumnUnit::Opaque,
-    &FrameMetrics::mLastXrDisplayTime>,
-  Column<
-    "Frame Interval"_cl,
-    ColumnUnit::Micros,
-    &FrameMetrics::mSincePreviousFrame>,
-  Column<
-    "FPS"_cl,
+    &FrameMetrics::mLastXrDisplayTime,
+  },
+  Column {
+    "Frame Interval",
+    &FrameMetrics::mSincePreviousFrame,
+  },
+  Column {
+    "FPS",
     ColumnUnit::Counter,
     [](const FrameMetrics& frame) {
       return 1.0e6 / frame.mSincePreviousFrame.count();
-    }>,
-  Column<"Count"_cl, ColumnUnit::Counter, &FrameMetrics::mFrameCount>,
-  Column<"App CPU"_cl, ColumnUnit::Micros, &FrameMetrics::mAppCpu>,
-  Column<"Render CPU"_cl, ColumnUnit::Micros, &FrameMetrics::mRenderCpu>,
-  Column<"Render GPU"_cl, ColumnUnit::Micros, &FrameMetrics::mRenderGpu>,
-  Column<"Wait CPU"_cl, ColumnUnit::Micros, &FrameMetrics::mWaitFrameCpu>,
-  Column<"Begin CPU"_cl, ColumnUnit::Micros, &FrameMetrics::mBeginFrameCpu>,
-  Column<"Submit CPU"_cl, ColumnUnit::Micros, &FrameMetrics::mEndFrameCpu>,
-  Column<
-    "VRAM Budget"_cl,
-    ColumnUnit::Bytes,
-    &DXGI_QUERY_VIDEO_MEMORY_INFO::Budget>,
-  Column<
-    "VRAM Current Usage "_cl,
-    ColumnUnit::Bytes,
-    &DXGI_QUERY_VIDEO_MEMORY_INFO::CurrentUsage>,
-  Column<
-    "VRAM Current Reserveration"_cl,
-    ColumnUnit::Bytes,
-    &DXGI_QUERY_VIDEO_MEMORY_INFO::CurrentReservation>,
-  Column<
-    "VRAM Available for Reserveration"_cl,
-    ColumnUnit::Bytes,
-    &DXGI_QUERY_VIDEO_MEMORY_INFO::AvailableForReservation>,
-  Column<
-    "GPU API"_cl,
+    },
+  },
+  Column {
+    "Count",
+    ColumnUnit::Counter,
+    &FrameMetrics::mFrameCount,
+  },
+  Column {
+    "App CPU",
+    &FrameMetrics::mAppCpu,
+  },
+  Column {
+    "Render CPU",
+    &FrameMetrics::mRenderCpu,
+  },
+  Column {
+    "Render GPU",
+    &FrameMetrics::mRenderGpu,
+  },
+  Column {
+    "Wait CPU",
+    &FrameMetrics::mWaitFrameCpu,
+  },
+  Column {
+    "Begin CPU",
+    &FrameMetrics::mBeginFrameCpu,
+  },
+  Column {
+    "Submit CPU",
+    &FrameMetrics::mEndFrameCpu,
+  },
+  Column {
+    "VRAM Budget",
+    &DXGI_QUERY_VIDEO_MEMORY_INFO::Budget,
+  },
+  Column {
+    "VRAM Current Usage ",
+    &DXGI_QUERY_VIDEO_MEMORY_INFO::CurrentUsage,
+  },
+  Column {
+    "VRAM Current Reserveration",
+    &DXGI_QUERY_VIDEO_MEMORY_INFO::CurrentReservation,
+  },
+  Column {
+    "VRAM Available for Reserveration",
+    &DXGI_QUERY_VIDEO_MEMORY_INFO::AvailableForReservation,
+  },
+  Column {
+    "GPU API",
     ColumnUnit::Opaque,
-    [](const FrameMetrics& frame) { return HasNVAPI(frame) ? "NVAPI" : ""; }>,
-  Column<
-    "GPU Clock Min"_cl,
+    [](const FrameMetrics& frame) { return HasNVAPI(frame) ? "NVAPI" : ""; },
+  },
+  Column {
+    "GPU Clock Min",
     ColumnUnit::KHz,
-    &FrameMetrics::mGpuGraphicsKHzMin>,
-  Column<
-    "GPU Clock Max"_cl,
+    &FrameMetrics::mGpuGraphicsKHzMin,
+  },
+  Column {
+    "GPU Clock Max",
     ColumnUnit::KHz,
-    &FrameMetrics::mGpuGraphicsKHzMax>,
-  Column<
-    "GPU VRAM Clock Min"_cl,
+    &FrameMetrics::mGpuGraphicsKHzMax,
+  },
+  Column {
+    "GPU VRAM Clock Min",
     ColumnUnit::KHz,
-    &FrameMetrics::mGpuMemoryKHzMin>,
-  Column<
-    "GPU VRAM Clock Max"_cl,
+    &FrameMetrics::mGpuMemoryKHzMin,
+  },
+  Column {
+    "GPU VRAM Clock Max",
     ColumnUnit::KHz,
-    &FrameMetrics::mGpuMemoryKHzMax>,
-  Column<
-    "GPU P-State Min"_cl,
+    &FrameMetrics::mGpuMemoryKHzMax,
+  },
+  Column {
+    "GPU P-State Min",
     ColumnUnit::Opaque,
-    &FrameMetrics::mGpuPStateMin>,
-  Column<
-    "GPU P-State Max"_cl,
+    &FrameMetrics::mGpuPStateMin,
+  },
+  Column {
+    "GPU P-State Max",
     ColumnUnit::Opaque,
-    &FrameMetrics::mGpuPStateMax>,
-  Column<
-    "GPU Limit Bits"_cl,
+    &FrameMetrics::mGpuPStateMax,
+  },
+  Column {
+    "GPU Limit Bits",
     ColumnUnit::Opaque,
     [](const FrameMetrics& frame) {
       return frame.mGpuPerformanceDecreaseReasons;
-    }>,
-  Column<
-    "GPU Thermal Limit"_cl,
+    },
+  },
+  Column {
+    "GPU Thermal Limit",
     ColumnUnit::Boolean,
     &HasAnyOfGPUPerfDecreaseBits<
-      NV_GPU_PERF_DECREASE_REASON_THERMAL_PROTECTION>>,
-  Column<
-    "GPU Power Limit"_cl,
+      NV_GPU_PERF_DECREASE_REASON_THERMAL_PROTECTION>,
+  },
+  Column {
+    "GPU Power Limit",
     ColumnUnit::Boolean,
     &HasAnyOfGPUPerfDecreaseBits<
       NV_GPU_PERF_DECREASE_REASON_POWER_CONTROL
       | NV_GPU_PERF_DECREASE_REASON_AC_BATT
-      | NV_GPU_PERF_DECREASE_REASON_INSUFFICIENT_POWER>>,
-  Column<
-    "GPU API Limit"_cl,
+      | NV_GPU_PERF_DECREASE_REASON_INSUFFICIENT_POWER>,
+  },
+  Column {
+    "GPU API Limit",
     ColumnUnit::Boolean,
-    &HasAnyOfGPUPerfDecreaseBits<NV_GPU_PERF_DECREASE_REASON_API_TRIGGERED>>>;
+    &HasAnyOfGPUPerfDecreaseBits<NV_GPU_PERF_DECREASE_REASON_API_TRIGGERED>,
+  },
+};
 
-std::string GetColumnHeaders() {
-  return []<std::size_t... I>(std::index_sequence<I...>) {
-    return std::array {std::tuple_element_t<I, Row>::GetHeader()...}
+std::string GetColumnHeaders(const auto& columns) {
+  return columns | std::views::transform(&Column::GetHeader)
     | std::views::join_with(","s) | std::ranges::to<std::string>();
-  }(std::make_index_sequence<std::tuple_size_v<Row>> {});
 }
 
-std::string GetRow(const FrameMetrics& frame) {
-  return [&frame]<std::size_t... I>(std::index_sequence<I...>) {
-    return std::array {std::tuple_element_t<I, Row>::GetValue(frame)...}
+std::string GetRow(const auto& columns, const FrameMetrics& frame) {
+  return columns | std::views::transform([&frame](const Column& column) {
+           return column.GetValue(frame);
+         })
     | std::views::join_with(","s) | std::ranges::to<std::string>();
-  }(std::make_index_sequence<std::tuple_size_v<Row>> {});
 }
 
 }// namespace
@@ -228,10 +268,14 @@ CSVWriter::Write(BinaryLogReader reader, HANDLE out, size_t framesPerRow) {
   const auto pcm = reader.GetPerformanceCounterMath();
   Result ret;
 
-  // Include the UTF-8 Byte Order Mark as Excel and Google Sheets use it
+  auto columns = std::vector<Column> {BaseColumns.begin(), BaseColumns.end()};
+
+  // Include the UTF-8 Byte Order Mark, because Excel and Google Sheets use it
   // as a magic value for UTF-8
   win32::println(
-    out, "\ufeffTime (µs),Time (UTC),Time (Local),{}", GetColumnHeaders());
+    out,
+    "\ufeffTime (µs),Time (UTC),Time (Local),{}",
+    GetColumnHeaders(columns));
 
   auto& frameCount = ret.mFrameCount;
   auto& flushCount = ret.mRowCount;
@@ -241,16 +285,17 @@ CSVWriter::Write(BinaryLogReader reader, HANDLE out, size_t framesPerRow) {
 
   const auto ToUTC = [clockCalibration = reader.GetClockCalibration(),
                       pcm](const LARGE_INTEGER& time) {
-    // As the binary logging happens in its' own thread, it's possible for the
-    // first few threads to have an end time that is earlier than the log start
-    // time
+    // As the binary logging happens in its' own thread, it's possible for
+    // the first few threads to have an end time that is earlier than the
+    // log start time
     const auto sinceCalibration = pcm.ToDurationAllowNegative(
       clockCalibration.mQueryPerformanceCounter, time);
     const auto sinceEpoch = sinceCalibration
       + std::chrono::microseconds(clockCalibration.mMicrosecondsSinceEpoch);
     static_assert(
       __cpp_lib_chrono >= 201907L,
-      "Need std::chrono::system_clock to be guaranteed to be UTC, using the "
+      "Need std::chrono::system_clock to be guaranteed to be UTC, using "
+      "the "
       "Unix epoch");
     return time_point_cast<std::chrono::milliseconds>(
       std::chrono::system_clock::time_point(sinceEpoch));
@@ -283,7 +328,7 @@ CSVWriter::Write(BinaryLogReader reader, HANDLE out, size_t framesPerRow) {
       pcm.ToDuration(*firstFrameTime, core.mEndFrameStop).count(),
       utc,
       localTime,
-      GetRow(*row));
+      GetRow(columns, *row));
     ++flushCount;
   }
 
