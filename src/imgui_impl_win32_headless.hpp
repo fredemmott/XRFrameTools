4200010f29a832cd62ffc23743a37a2496711cac
Fred Emmott <git@fredemmott.com>
2025-10-01 08:45:01 -0500

    🔥 Remove no-longer-used headless imgui code

--- src/imgui_impl_win32_headless.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 Fred Emmott <fred@fredemmott.com>
-// SPDX-License-Identifier: MIT
-#pragma once
-
-#include <imgui.h>
-
-void ImGui_ImplWin32_Headless_Init();
-void ImGui_ImplWin32_Headless_Shutdown();
-void ImGui_ImplWin32_Headless_NewFrame(ImVec2 size);
